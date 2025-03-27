e25324617fa3571f31573d79ca6a299b64c48a56
clarissarichard <107963214+clarissarichard@users.noreply.github.com>
2025-03-27 10:05:24 -0600

    Create branch and add PDF test

---
+++ playwright/tests/pdf-reader-navigation.spec.ts
@@ -0,0 +1,16 @@
+import { test, expect } from '@playwright/test';
+import { PdfReaderPage } from '../pageobjects/web-reader.page.ts';
+
+test.describe('Test PDF pub', () => {
+  test('Displays reader navigation in PDF pub', async ({ page }) => {
+    const pdfReaderPage = new PdfReaderPage(page);
+    await page.goto('/pdf/collection');
+    await expect(pdfReaderPage.backButton).toBeVisible();
+    await expect(pdfReaderPage.nextPageButton).toBeVisible();
+    await expect(pdfReaderPage.previousPageButton).toBeVisible();
+    await expect(pdfReaderPage.tocButton).toBeVisible();
+    await pdfReaderPage.tocButton.click();
+    await expect(pdfReaderPage.firstChapter).toBeVisible();
+    await expect(pdfReaderPage.lastChapter).toBeVisible();
+  });
+});
