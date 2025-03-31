6f07df08ac62907fec8ade268f3c327309cc0440
clarissarichard <107963214+clarissarichard@users.noreply.github.com>
2025-03-31 09:50:52 -0600

    Added navigation in full screen test

--- playwright/tests/html-reader-navigation.spec.ts
+++ playwright/tests/html-reader-navigation.spec.ts
@@ -113,174 +113,204 @@ test.describe('Test navigation in HTML pub', () => {
     await expect(htmlReaderPage.nextPageButton).toBeVisible();
     await expect(htmlReaderPage.nextPageButton).toBeDisabled();
   });
-});
 
-test('Scroll to the bottom of the page', async ({ page }) => {
-  const htmlReaderPage = new HtmlReaderPage(page);
-  await htmlReaderPage.loadPage('/html/moby-epub3');
-  await expect(htmlReaderPage.settingsButton).toBeVisible();
-  await htmlReaderPage.settingsButton.click();
-  await expect(htmlReaderPage.scrollingStyle).toBeVisible();
-  await htmlReaderPage.scrollingStyle.click();
-  await expect(htmlReaderPage.scrollingStyle).toBeChecked();
-  await expect(htmlReaderPage.tocButton).toBeVisible();
-  await htmlReaderPage.tocButton.click();
-  await expect(htmlReaderPage.chapterName).toBeVisible();
-  await htmlReaderPage.chapterName.click();
-  await htmlReaderPage.scrollDown();
-});
+  test('Scroll to the bottom of the page', async ({ page }) => {
+    const htmlReaderPage = new HtmlReaderPage(page);
+    await htmlReaderPage.loadPage('/html/moby-epub3');
+    await expect(htmlReaderPage.settingsButton).toBeVisible();
+    await htmlReaderPage.settingsButton.click();
+    await expect(htmlReaderPage.scrollingStyle).toBeVisible();
+    await htmlReaderPage.scrollingStyle.click();
+    await expect(htmlReaderPage.scrollingStyle).toBeChecked();
+    await expect(htmlReaderPage.tocButton).toBeVisible();
+    await htmlReaderPage.tocButton.click();
+    await expect(htmlReaderPage.chapterName).toBeVisible();
+    await htmlReaderPage.chapterName.click();
+    await htmlReaderPage.scrollDown();
+  });
 
-test('Scroll to the top of the page', async ({ page }) => {
-  const htmlReaderPage = new HtmlReaderPage(page);
-  await htmlReaderPage.loadPage('/html/moby-epub3');
-  await expect(htmlReaderPage.settingsButton).toBeVisible();
-  await htmlReaderPage.settingsButton.click();
-  await expect(htmlReaderPage.scrollingStyle).toBeVisible();
-  await htmlReaderPage.scrollingStyle.click();
-  await expect(htmlReaderPage.scrollingStyle).toBeChecked();
-  await expect(htmlReaderPage.tocButton).toBeVisible();
-  await htmlReaderPage.tocButton.click();
-  await expect(htmlReaderPage.chapterName).toBeVisible();
-  await htmlReaderPage.chapterName.click();
-  await htmlReaderPage.scrollUp();
-});
+  test('Scroll to the top of the page', async ({ page }) => {
+    const htmlReaderPage = new HtmlReaderPage(page);
+    await htmlReaderPage.loadPage('/html/moby-epub3');
+    await expect(htmlReaderPage.settingsButton).toBeVisible();
+    await htmlReaderPage.settingsButton.click();
+    await expect(htmlReaderPage.scrollingStyle).toBeVisible();
+    await htmlReaderPage.scrollingStyle.click();
+    await expect(htmlReaderPage.scrollingStyle).toBeChecked();
+    await expect(htmlReaderPage.tocButton).toBeVisible();
+    await htmlReaderPage.tocButton.click();
+    await expect(htmlReaderPage.chapterName).toBeVisible();
+    await htmlReaderPage.chapterName.click();
+    await htmlReaderPage.scrollUp();
+  });
 
-// navigate with full screen
+  test('Navigate reader in full screen', async ({ page }) => {
+    const htmlReaderPage = new HtmlReaderPage(page);
+    await htmlReaderPage.loadPage('/html/moby-epub3');
+    await expect(htmlReaderPage.fullScreenButton).toBeVisible();
+    await htmlReaderPage.fullScreenButton.click();
+    await expect(htmlReaderPage.nextPageButton).toBeVisible();
+    await expect(htmlReaderPage.nextPageButton).toBeEnabled();
+    await expect(htmlReaderPage.previousPageButton).toBeVisible();
+    await expect(htmlReaderPage.previousPageButton).toBeDisabled();
+    await htmlReaderPage.nextPageButton.click();
+    await expect(htmlReaderPage.nextPageButton).toBeVisible();
+    await expect(htmlReaderPage.nextPageButton).toBeEnabled();
+    await expect(htmlReaderPage.previousPageButton).toBeVisible();
+    await expect(htmlReaderPage.previousPageButton).toBeEnabled();
+    await htmlReaderPage.previousPageButton.click();
+    await expect(htmlReaderPage.nextPageButton).toBeVisible();
+    await expect(htmlReaderPage.nextPageButton).toBeEnabled();
+    await expect(htmlReaderPage.previousPageButton).toBeVisible();
+    await expect(htmlReaderPage.previousPageButton).toBeDisabled();
+    await expect(htmlReaderPage.settingsButton).toBeVisible();
+    await htmlReaderPage.settingsButton.click();
+    await expect(htmlReaderPage.scrollingStyle).toBeVisible();
+    await htmlReaderPage.scrollingStyle.click();
+    await expect(htmlReaderPage.tocButton).toBeVisible();
+    await htmlReaderPage.tocButton.click();
+    await expect(htmlReaderPage.chapterName).toBeVisible();
+    await htmlReaderPage.chapterName.click();
+    await expect(htmlReaderPage.chapterHeading).toBeVisible();
+    await htmlReaderPage.scrollDown();
+    await htmlReaderPage.scrollUp();
+  });
 
-test('Navigate reader with changed screen size', async ({ page }) => {
-  const htmlReaderPage = new HtmlReaderPage(page);
-  await htmlReaderPage.loadPage('/html/moby-epub3');
-  await htmlReaderPage.changeScreenSize();
-  await expect(htmlReaderPage.nextPageButton).toBeVisible();
-  await expect(htmlReaderPage.nextPageButton).toBeEnabled();
-  await expect(htmlReaderPage.previousPageButton).toBeVisible();
-  await expect(htmlReaderPage.previousPageButton).toBeDisabled();
-  await htmlReaderPage.nextPageButton.click();
-  await expect(htmlReaderPage.nextPageButton).toBeVisible();
-  await expect(htmlReaderPage.nextPageButton).toBeEnabled();
-  await expect(htmlReaderPage.previousPageButton).toBeVisible();
-  await expect(htmlReaderPage.previousPageButton).toBeEnabled();
-  await htmlReaderPage.previousPageButton.click();
-  await expect(htmlReaderPage.nextPageButton).toBeVisible();
-  await expect(htmlReaderPage.nextPageButton).toBeEnabled();
-  await expect(htmlReaderPage.previousPageButton).toBeVisible();
-  await expect(htmlReaderPage.previousPageButton).toBeDisabled();
-  await expect(htmlReaderPage.settingsButton).toBeVisible();
-  await htmlReaderPage.settingsButton.click();
-  await expect(htmlReaderPage.scrollingStyle).toBeVisible();
-  await htmlReaderPage.scrollingStyle.click();
-  await expect(htmlReaderPage.tocButton).toBeVisible();
-  await htmlReaderPage.tocButton.click();
-  await expect(htmlReaderPage.chapterName).toBeVisible();
-  await htmlReaderPage.chapterName.click();
-  await expect(htmlReaderPage.chapterHeading).toBeVisible();
-  await htmlReaderPage.scrollDown();
-  await htmlReaderPage.scrollUp();
-});
+  test('Navigate reader with changed screen size', async ({ page }) => {
+    const htmlReaderPage = new HtmlReaderPage(page);
+    await htmlReaderPage.loadPage('/html/moby-epub3');
+    await htmlReaderPage.changeScreenSize();
+    await expect(htmlReaderPage.nextPageButton).toBeVisible();
+    await expect(htmlReaderPage.nextPageButton).toBeEnabled();
+    await expect(htmlReaderPage.previousPageButton).toBeVisible();
+    await expect(htmlReaderPage.previousPageButton).toBeDisabled();
+    await htmlReaderPage.nextPageButton.click();
+    await expect(htmlReaderPage.nextPageButton).toBeVisible();
+    await expect(htmlReaderPage.nextPageButton).toBeEnabled();
+    await expect(htmlReaderPage.previousPageButton).toBeVisible();
+    await expect(htmlReaderPage.previousPageButton).toBeEnabled();
+    await htmlReaderPage.previousPageButton.click();
+    await expect(htmlReaderPage.nextPageButton).toBeVisible();
+    await expect(htmlReaderPage.nextPageButton).toBeEnabled();
+    await expect(htmlReaderPage.previousPageButton).toBeVisible();
+    await expect(htmlReaderPage.previousPageButton).toBeDisabled();
+    await expect(htmlReaderPage.settingsButton).toBeVisible();
+    await htmlReaderPage.settingsButton.click();
+    await expect(htmlReaderPage.scrollingStyle).toBeVisible();
+    await htmlReaderPage.scrollingStyle.click();
+    await expect(htmlReaderPage.tocButton).toBeVisible();
+    await htmlReaderPage.tocButton.click();
+    await expect(htmlReaderPage.chapterName).toBeVisible();
+    await htmlReaderPage.chapterName.click();
+    await expect(htmlReaderPage.chapterHeading).toBeVisible();
+    await htmlReaderPage.scrollDown();
+    await htmlReaderPage.scrollUp();
+  });
 
-test('Click internal link in paginated mode', async ({ page }) => {
-  const htmlReaderPage = new HtmlReaderPage(page);
-  await htmlReaderPage.loadPage('/html/moby-epub3');
-  await expect(htmlReaderPage.nextPageButton).toBeVisible();
-  await expect(htmlReaderPage.nextPageButton).toBeEnabled();
-  await expect(htmlReaderPage.previousPageButton).toBeVisible();
-  await expect(htmlReaderPage.previousPageButton).toBeDisabled();
-  await htmlReaderPage.nextPageButton.click();
-  await expect(htmlReaderPage.internalLink).toBeVisible();
-  await htmlReaderPage.internalLink.click();
-  await expect(htmlReaderPage.titlePage).toBeVisible();
-});
+  test('Click internal link in paginated mode', async ({ page }) => {
+    const htmlReaderPage = new HtmlReaderPage(page);
+    await htmlReaderPage.loadPage('/html/moby-epub3');
+    await expect(htmlReaderPage.nextPageButton).toBeVisible();
+    await expect(htmlReaderPage.nextPageButton).toBeEnabled();
+    await expect(htmlReaderPage.previousPageButton).toBeVisible();
+    await expect(htmlReaderPage.previousPageButton).toBeDisabled();
+    await htmlReaderPage.nextPageButton.click();
+    await expect(htmlReaderPage.internalLink).toBeVisible();
+    await htmlReaderPage.internalLink.click();
+    await expect(htmlReaderPage.titlePage).toBeVisible();
+  });
 
-test('Click internal link in scrolling mode', async ({ page }) => {
-  const htmlReaderPage = new HtmlReaderPage(page);
-  await htmlReaderPage.loadPage('/html/moby-epub3');
-  await htmlReaderPage.settingsButton.click();
-  await htmlReaderPage.scrollingStyle.click();
-  await expect(htmlReaderPage.nextPageButton).toBeVisible();
-  await expect(htmlReaderPage.nextPageButton).toBeEnabled();
-  await expect(htmlReaderPage.previousPageButton).toBeVisible();
-  await expect(htmlReaderPage.previousPageButton).toBeDisabled();
-  await htmlReaderPage.nextPageButton.click();
-  await expect(htmlReaderPage.internalLink).toBeVisible();
-  await htmlReaderPage.internalLink.click();
-  await expect(htmlReaderPage.titlePage).toBeVisible();
-});
+  test('Click internal link in scrolling mode', async ({ page }) => {
+    const htmlReaderPage = new HtmlReaderPage(page);
+    await htmlReaderPage.loadPage('/html/moby-epub3');
+    await htmlReaderPage.settingsButton.click();
+    await htmlReaderPage.scrollingStyle.click();
+    await expect(htmlReaderPage.nextPageButton).toBeVisible();
+    await expect(htmlReaderPage.nextPageButton).toBeEnabled();
+    await expect(htmlReaderPage.previousPageButton).toBeVisible();
+    await expect(htmlReaderPage.previousPageButton).toBeDisabled();
+    await htmlReaderPage.nextPageButton.click();
+    await expect(htmlReaderPage.internalLink).toBeVisible();
+    await htmlReaderPage.internalLink.click();
+    await expect(htmlReaderPage.titlePage).toBeVisible();
+  });
 
-test('Click external link in paginated mode', async ({ page }) => {
-  const htmlReaderPage = new HtmlReaderPage(page);
-  await htmlReaderPage.loadPage('/html/moby-epub3');
-  await expect(htmlReaderPage.tocButton).toBeVisible();
-  await htmlReaderPage.tocButton.click();
-  await expect(htmlReaderPage.lastChapter).toBeVisible();
-  await htmlReaderPage.lastChapter.click();
-  await expect(htmlReaderPage.externalLink).toBeVisible();
-  await htmlReaderPage.externalLink.click();
-  const openLink = page.waitForEvent('popup');
-  const newTab = await openLink;
-  await newTab.waitForLoadState();
-  await expect(newTab).toHaveURL('https://www.gutenberg.org');
-});
+  test('Click external link in paginated mode', async ({ page }) => {
+    const htmlReaderPage = new HtmlReaderPage(page);
+    await htmlReaderPage.loadPage('/html/moby-epub3');
+    await expect(htmlReaderPage.tocButton).toBeVisible();
+    await htmlReaderPage.tocButton.click();
+    await expect(htmlReaderPage.lastChapter).toBeVisible();
+    await htmlReaderPage.lastChapter.click();
+    await expect(htmlReaderPage.externalLink).toBeVisible();
+    await htmlReaderPage.externalLink.click();
+    const openLink = page.waitForEvent('popup');
+    const newTab = await openLink;
+    await newTab.waitForLoadState();
+    await expect(newTab).toHaveURL('https://www.gutenberg.org');
+  });
 
-test('Remember last location when exit and reenter reader', async ({
-  page,
-}) => {
-  const htmlReaderPage = new HtmlReaderPage(page);
-  await htmlReaderPage.loadPage('/html/moby-epub3');
-  await expect(htmlReaderPage.tocButton).toBeVisible();
-  await htmlReaderPage.tocButton.click();
-  await htmlReaderPage.chapterName.click();
-  await expect(htmlReaderPage.chapterHeading).toBeVisible();
-  await expect(htmlReaderPage.backButton).toBeVisible();
-  await htmlReaderPage.backButton.click();
-  await expect(htmlReaderPage.webReaderHomepage).toBeVisible();
-  await htmlReaderPage.loadPage('/html/moby-epub3');
-  await expect(htmlReaderPage.chapterHeading).toBeVisible();
-});
+  test('Remember last location when exit and reenter reader', async ({
+    page,
+  }) => {
+    const htmlReaderPage = new HtmlReaderPage(page);
+    await htmlReaderPage.loadPage('/html/moby-epub3');
+    await expect(htmlReaderPage.tocButton).toBeVisible();
+    await htmlReaderPage.tocButton.click();
+    await htmlReaderPage.chapterName.click();
+    await expect(htmlReaderPage.chapterHeading).toBeVisible();
+    await expect(htmlReaderPage.backButton).toBeVisible();
+    await htmlReaderPage.backButton.click();
+    await expect(htmlReaderPage.webReaderHomepage).toBeVisible();
+    await htmlReaderPage.loadPage('/html/moby-epub3');
+    await expect(htmlReaderPage.chapterHeading).toBeVisible();
+  });
 
-test('Remember last location when visit another remembering pub and reenter original pub', async ({
-  page,
-}) => {
-  const htmlReaderPage = new HtmlReaderPage(page);
-  await htmlReaderPage.loadPage('/html/moby-epub3');
-  await expect(htmlReaderPage.tocButton).toBeVisible();
-  await htmlReaderPage.tocButton.click();
-  await htmlReaderPage.chapterName.click();
-  await expect(htmlReaderPage.chapterHeading).toBeVisible();
-  await expect(htmlReaderPage.backButton).toBeVisible();
-  await htmlReaderPage.backButton.click();
-  await expect(htmlReaderPage.webReaderHomepage).toBeVisible();
-  await htmlReaderPage.loadPage('/html/moby-epub2');
-  await expect(htmlReaderPage.epubCover).toBeVisible();
-  await htmlReaderPage.backButton.click();
-  await expect(htmlReaderPage.webReaderHomepage).toBeVisible();
-  await htmlReaderPage.loadPage('/html/moby-epub3');
-  await expect(htmlReaderPage.chapterHeading).toBeVisible();
-});
+  test('Remember last location when visit another remembering pub and reenter original pub', async ({
+    page,
+  }) => {
+    const htmlReaderPage = new HtmlReaderPage(page);
+    await htmlReaderPage.loadPage('/html/moby-epub3');
+    await expect(htmlReaderPage.tocButton).toBeVisible();
+    await htmlReaderPage.tocButton.click();
+    await htmlReaderPage.chapterName.click();
+    await expect(htmlReaderPage.chapterHeading).toBeVisible();
+    await expect(htmlReaderPage.backButton).toBeVisible();
+    await htmlReaderPage.backButton.click();
+    await expect(htmlReaderPage.webReaderHomepage).toBeVisible();
+    await htmlReaderPage.loadPage('/html/moby-epub2');
+    await expect(htmlReaderPage.epubCover).toBeVisible();
+    await htmlReaderPage.backButton.click();
+    await expect(htmlReaderPage.webReaderHomepage).toBeVisible();
+    await htmlReaderPage.loadPage('/html/moby-epub3');
+    await expect(htmlReaderPage.chapterHeading).toBeVisible();
+  });
 
-test('Do not remember last location when exit and reenter reader in specific pub', async ({
-  page,
-}) => {
-  const htmlReaderPage = new HtmlReaderPage(page);
-  await htmlReaderPage.loadPage('/html/moby-epub3-no-local-storage');
-  await expect(htmlReaderPage.tocButton).toBeVisible();
-  await htmlReaderPage.tocButton.click();
-  await htmlReaderPage.chapterName.click();
-  await expect(htmlReaderPage.chapterHeading).toBeVisible();
-  await expect(htmlReaderPage.backButton).toBeVisible();
-  await htmlReaderPage.backButton.click();
-  await expect(htmlReaderPage.webReaderHomepage).toBeVisible();
-  await htmlReaderPage.loadPage('/html/moby-epub3-no-local-storage');
-  await expect(htmlReaderPage.titlePage).toBeVisible();
-  await expect(htmlReaderPage.chapterHeading).not.toBeVisible();
-});
+  test('Do not remember last location when exit and reenter reader in specific pub', async ({
+    page,
+  }) => {
+    const htmlReaderPage = new HtmlReaderPage(page);
+    await htmlReaderPage.loadPage('/html/moby-epub3-no-local-storage');
+    await expect(htmlReaderPage.tocButton).toBeVisible();
+    await htmlReaderPage.tocButton.click();
+    await htmlReaderPage.chapterName.click();
+    await expect(htmlReaderPage.chapterHeading).toBeVisible();
+    await expect(htmlReaderPage.backButton).toBeVisible();
+    await htmlReaderPage.backButton.click();
+    await expect(htmlReaderPage.webReaderHomepage).toBeVisible();
+    await htmlReaderPage.loadPage('/html/moby-epub3-no-local-storage');
+    await expect(htmlReaderPage.titlePage).toBeVisible();
+    await expect(htmlReaderPage.chapterHeading).not.toBeVisible();
+  });
 
-test('Confirm missing TOC in specific pub', async ({ page }) => {
-  const htmlReaderPage = new HtmlReaderPage(page);
-  await htmlReaderPage.loadPage('/html/test/missing-toc');
-  await expect(htmlReaderPage.tocButton).toBeVisible();
-  await htmlReaderPage.tocButton.click();
-  const missingTOC = page.getByText(
-    'This publication does not have a Table of Contents.'
-  );
-  await expect(missingTOC).toBeVisible();
+  test('Confirm missing TOC in specific pub', async ({ page }) => {
+    const htmlReaderPage = new HtmlReaderPage(page);
+    await htmlReaderPage.loadPage('/html/test/missing-toc');
+    await expect(htmlReaderPage.tocButton).toBeVisible();
+    await htmlReaderPage.tocButton.click();
+    const missingTOC = page.getByText(
+      'This publication does not have a Table of Contents.'
+    );
+    await expect(missingTOC).toBeVisible();
+  });
 });
