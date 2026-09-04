2fa85b3c5cf009fab785e2eeba0e776aaea7abfc
Jackie Quach <jackiequach14@gmail.com>
2026-09-04 14:41:27 -0400

    enable range requests for linearized pdfs

--- src/PdfReader/constants.ts
+++ src/PdfReader/constants.ts
@@ -1,4 +1,5 @@
 // Constants specific to PdfReader (zoom limits, layout spacing, scroll behavior).
+import { PageSize } from './types';
 
 export const MIN_SCALE = 0.25;
 export const MAX_SCALE = 5;
@@ -6,4 +7,6 @@ export const SCALE_STEP = 0.1;
 export const PAGE_GAP = 16;
 export const PAGE_PADDING = 32;
 export const RENDER_ROOT_MARGIN = '300px 0px 300px 0px';
+export const PAGE_MEASURE_BUFFER = 2;
+export const DEFAULT_PAGE_SIZE: PageSize = { width: 612, height: 792 };
 export const SCROLLSPY_ANCHOR_RATIO = 0.3;
